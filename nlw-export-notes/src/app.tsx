5bd889078c4e2225fc09c1bad0c0d90fdbf48b55
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2024-02-13 13:58:53 -0300

    Final
    Finalizando a aplicação

--- nlw-export-notes/src/app.tsx
+++ nlw-export-notes/src/app.tsx
@@ -1,29 +1,60 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
 export function App() {
 
-  const [notes, setNotes] = useState([
-    {id: 1, date: new Date(),  content: 'Hello World'},
-    {id: 2, date: new Date(),  content: 'Hello World 2'}
-  ])
+  const [search, setSearch] = useState('')
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem("notes");
 
-  function onNoteCreated(content: string){
-    const newnote = {
-      id: Math.random(),
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage);
+    }
+    return [];
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
       date: new Date(),
       content,
+    };
 
-    }
+    const notesArray = [newNote, ...notes];
 
-    setNotes([newnote,...notes])
+    setNotes(notesArray);
 
+    localStorage.setItem("notes", JSON.stringify(notesArray));
   }
 
+  function onnoteDeleted(id: string){
+    const notesArray = notes.filter(note => {
+      return note.id != id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>){
+    const query = event.target.value
+    setSearch(query)
+  }
+
+  const filterNotes = search != ''
+    ? notes.filter(note => note.content.includes(search)) : notes
+
+    console.log("Notes:", notes);
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="nlw expert" />
 
       <form className="w-full">
@@ -31,16 +62,16 @@ export function App() {
           type="text"
           placeholder="Busque em suas notaas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard onNoteCreated={onNoteCreated}/>
-        {notes.map(note => {
-          return <NoteCard key={note.id} note={note} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {filterNotes.map(note => {
+          return <NoteCard key={note.id} note={note}/>
         })}
-       
       </div>
     </div>
   );
--- nlw-export-notes/src/components/new-note-card.tsx
+++ nlw-export-notes/src/components/new-note-card.tsx
@@ -3,30 +3,81 @@ import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
-interface NewNoteCardProps{
-  onNoteCreated: (content: string) => void
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void;
 }
 
-export function NewNoteCard( {onNoteCreated}: NewNoteCardProps) {
+
+let speechRecognition: SpeechRecognition | null = null
+
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true);
-  const [content, setContent] = useState('')
+  const [isRecording, setIsRecording] = useState(false);
+  const [content, setContent] = useState("");
 
   function handleStartEditor() {
     setShouldShowOnBoarding(false);
   }
 
   function handleContextChange(event: ChangeEvent<HTMLTextAreaElement>) {
-    setContent(event.target.value)
+    setContent(event.target.value);
 
     if (event.target.value === "") {
       setShouldShowOnBoarding(true);
     }
   }
 
-  function handleSaveNote(event: FormEvent){
-    event.preventDefault()
-    onNoteCreated(content)
-    toast.success('Nota criada com sucesso!')
+  function handleSaveNote(event: FormEvent) {
+    if (content == ''){
+      return
+    }
+    event.preventDefault();
+    onNoteCreated(content);
+    setContent("");
+    setShouldShowOnBoarding(true);
+    toast.success("Nota criada com sucesso!");
+  }
+
+  function handleStartRecording() {
+    setIsRecording(true)
+
+    const isSpeechRecognitionAPIAvailable = 'SpeechRecognition' in window || 'webkitSpeechRecognition' in window
+    if (!isSpeechRecognitionAPIAvailable){
+      alert('Infelizmente seu navegador não suporta a API de gravação')
+      return
+    }
+
+
+    const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition
+    speechRecognition =  new SpeechRecognitionAPI()
+
+    speechRecognition.lang = 'pt-Br'
+    speechRecognition.continuous = true
+    speechRecognition.maxAlternatives = 1
+    speechRecognition.interimResults =  true
+
+    speechRecognition.onresult = (event) => {
+      const transcription =  Array.from(event.results).reduce((text, result) => {
+
+        return text.concat(result[0].transcript)
+      }, '')
+      setContent(transcription)
+    }
+
+    speechRecognition.onerror = (event) => {
+      console.error(event)
+    }
+
+    speechRecognition.start()
+  }
+
+  function handleStopRecording(){
+    setIsRecording(false)
+
+    if(speechRecognition != null){
+      speechRecognition.stop()
+    }
   }
 
   return (
@@ -41,23 +92,28 @@ export function NewNoteCard( {onNoteCreated}: NewNoteCardProps) {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[648px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[648px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5 "> </X>
           </Dialog.Close>
-          <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+          <form className="flex-1 flex flex-col">
             <div className="flex flex-1 flex-col gap-3 p-5">
               <span className="text-sm font-medium text-slate-300">
                 Adicionar nota
               </span>
               {shouldShowOnBoarding ? (
                 <p className="text-sm leading-6 text-slate-400">
-                  Comece{" "}
-                  <button className="font-medium text-lime-400 hover:underline">
-                    gravando uma nota
-                  </button>{" "}
-                  em áudio ou se preferir{" "}
+                  Comece
                   <button
+                    type="button"
+                    onClick={handleStartRecording}
+                    className="font-medium text-lime-400 hover:underline"
+                  >
+                    gravando uma nota
+                  </button>
+                  em áudio ou se preferir
+                  <button
+                    type="button"
                     onClick={handleStartEditor}
                     className="font-medium text-lime-400 hover:underline"
                   >
@@ -70,15 +126,28 @@ export function NewNoteCard( {onNoteCreated}: NewNoteCardProps) {
                   autoFocus
                   className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                   onChange={handleContextChange}
+                  value={content}
                 ></textarea>
               )}
             </div>
-            <button
-              type="submit"
-              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
-            >
-              <span className="">Salvar nota</span>
-            </button>
+            {isRecording ? (
+              <button
+                type="button"
+                onClick={handleStopRecording}
+                className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
+              >
+                <div className="size-3 rounded-full bg-red-500 animate-pulse"></div>
+                <span className="">Gravando! (clique p/ interromper)</span>
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={handleSaveNote}
+                className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+              >
+                <span className="">Salvar nota</span>
+              </button>
+            )}
           </form>
         </Dialog.Content>
       </Dialog.Portal>
